04a4ea7531eafc6b9957c54dbb978f4ef038d803
759781714@qq.com <759781714@qq.com>
2016-12-09 11:06:08 +0800

    控制浏览器前进后退

---
+++ backforward.py
@@ -0,0 +1,32 @@
+#coding=utf-8
+
+from selenium import webdriver
+import time
+driver = webdriver.Chrome()
+
+#访问百度首页
+
+
+first_url = "http://www.baidu.com"
+print "now access %s" %(first_url)
+driver.get(first_url)
+
+#访问新闻页面
+
+second_url = "http://news.baidu.com"
+print "now access %s" %(second_url)
+driver.get(second_url)
+
+#返回到百度首页
+print "back to %s" %(first_url)
+driver.back()
+time.sleep(2)
+#前进到新闻页
+
+print "forward to %s" %(second_url)
+driver.forward()
+
+time.sleep(5)
+
+driver.quit()
+
