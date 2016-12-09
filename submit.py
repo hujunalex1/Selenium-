05ac79d3872118eec98329d01c7767ab8f01f955
759781714@qq.com <759781714@qq.com>
2016-12-09 13:15:09 +0800

    submit()用法

---
+++ submit.py
@@ -0,0 +1,14 @@
+#coding=utf-8
+from selenium import webdriver
+import time
+driver = webdriver.Chrome()
+driver.get("http://youdao.com/")
+driver.find_element_by_id("translateContent").send_keys("hello")
+
+#submit()方法用于提交表单，这里特别用于没提交按钮的情况，例如搜索框输入关键字之后的“回车”
+driver.find_element_by_css_selector("input[name='q']").submit()
+
+driver.quit()
+
+
+
