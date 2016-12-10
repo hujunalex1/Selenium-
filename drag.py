315f4f9dcc31667b19463a5d6af433162dcfd259
759781714@qq.com <759781714@qq.com>
2016-12-10 16:29:32 +0800

    新增鼠标拖拽方法

---
+++ drag.py
@@ -0,0 +1,10 @@
+#coding=utf-8
+
+from  selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+driver = webdriver.Firefox()
+driver.get("http://www.baidu.com")
+
+element=driver.find_element_by_xpath("//*[@id='lg']/img")
+target = driver.find_element_by_id("kw")
+ActionChains(driver).drag_and_drop(element,target).perform()
