baee40c44411526cead889eaa77cb780aa31bfef
759781714@qq.com <759781714@qq.com>
2016-12-13 10:27:21 +0800

    多组元素定位

--- checkbox.py
+++ checkbox.py
@@ -7,7 +7,7 @@ file = os.path.abspath("c:\\Temp\\checkbox.html") #获取文件路径
 print file
 driver.get(file)
 
-# 选择页面上所有的input 的元素
+# 选择页面上所有的input 的元素！
 inputs = driver.find_elements_by_css_selector("input[type='checkbox']")
 
 #然后从中过滤出tpye 为checkbox 的元素，单击勾选
@@ -16,6 +16,7 @@ for i in inputs:
         i.click()
 
 print len("checkbox")
+
 time.sleep(4)
 driver.quit()
 
