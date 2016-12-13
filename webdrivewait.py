d392472b254de66b6f6f46ce7c02568c5fbcd178
759781714@qq.com <759781714@qq.com>
2016-12-13 10:23:43 +0800

    优化代码

--- webdrivewait.py
+++ webdrivewait.py
@@ -6,10 +6,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
-driver.implicitly_wait(10)
+driver.implicitly_wait(10) #隐式等待方法
 driver.get("http://www.baidu.com")
 
-#element = WebDriverWait(driver,5,0.5).until(EC.presence_of_element_located((By.ID,"kw")))
+#element = WebDriverWait(driver,5,0.5).until(EC.presence_of_element_located((By.ID,"kw")))#显示等待方法
 #element.send_keys("selenium")
 
 driver.find_element_by_id("kw").send_keys("selenium")
+driver.quit()
