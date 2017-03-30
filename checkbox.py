cff0c18522ccbac23690b68892262cf360425155
hujun <759781714@qq.com>
2017-03-30 14:29:35 +0800

    优化代码

--- checkbox.py
+++ checkbox.py
@@ -7,18 +7,18 @@ driver = webdriver.Chrome()
 file = os.path.abspath("c:\\Temp\\checkbox.html") #获取文件路径
 print file
 driver.get(file)
-
 # 选择页面上所有的input 的元素！
-inputs = driver.find_elements_by_css_selector("input[type='checkbox']")
+inputs = driver.find_elements_by_css_selector("input[type=checkbox]")
 
 #然后从中过滤出tpye 为checkbox 的元素，单击勾选
 for i in inputs:
     if i.get_attribute("type") == "checkbox":
         i.click()
 
-print len("checkbox")
-
-time.sleep(4)
+print len(inputs)
+# 把页面上最后1个checkbox的勾给去掉
+driver.find_elements_by_css_selector("input[type='checkbox']").pop().click()
+time.sleep(2)
 driver.quit()
 
 
