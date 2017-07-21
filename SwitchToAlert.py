6f2b4ceb8b44f567d1d0128785bcf2c0f72434ef
hujun <759781714@qq.com>
2017-07-21 13:39:34 +0800

    实现点击保存按钮

--- SwitchToAlert.py
+++ SwitchToAlert.py
@@ -10,12 +10,13 @@ ActionChains(driver).move_to_element(link).perform()
 
 #点击搜索设置按钮
 driver.find_element_by_class_name("setpref").click()
-
-#点击保存按钮
-driver.find_element_by_css_selector("#gxszButton > a.prefpanelgo").click()
-
-#接收弹窗
-driver.switch_to.alert().accept()
+time.sleep(2)
+#切换页面句柄
+for handle in driver.window_handles:
+    driver.switch_to.window(handle)
+time.sleep(2)
+#点击保存设置按钮
+driver.find_element_by_id("gxszButton").find_element_by_class_name("prefpanelgo").click()
 
 time.sleep(3)
 driver.quit()
